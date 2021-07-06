d3f73c9991021957f1829e0be6370ca303a837ac
DEISEMACIEL <85131772+DEISEMACIEL@users.noreply.github.com>
2021-07-05 21:47:22 -0300

    Add files via upload

---
+++ Projeto-a/js/index.js
@@ -0,0 +1,34 @@
+var nome = "Deise Soares Morais";
+var cargo = "CTO-Growdev";
+
+var nomeHtml = document.getElementById("nome-no-html");
+var CargonoHtml = document.getElementById("cargo-no-html");
+var texto2 = document.getElementById("texto-1");
+var texto1 = document.getElementById("texto-2")
+
+function
+colocarNomeNoHtml(nome) {
+    nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo) {
+    nomeHtml.innerHTML = cargo;
+}
+
+function logarNome() {
+    console.log(nome);
+}
+
+function clickNoProjetos() {
+    console.log("clicou no botão projetos");
+    texto2.style.display = "block";
+    texto1.style.display = "none;"
+}
+
+function clickNoSobre() {
+    console.log("clicou no botãos");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
